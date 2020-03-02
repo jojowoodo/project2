5a0f5070948ed1bb152e6055db6c87ef2660e29d
jojowoodo <jojowoodo@yahoo.com>
2020-03-02 12:57:35 +0200

    Kopiera in bankarr openfile och satte in alla språkfiler

---
+++ project2/src/com/company/readin.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.io.*;
+import java.util.HashMap;
+
+
+//Kopiera från bankappen
+public class readin {
+    public static HashMap<Integer> openFromFile() {
+
+
+        try {
+
+            FileInputStream fIn = new FileInputStream(new File("AccountDb"));
+            ObjectInputStream oIn = new ObjectInputStream(fIn);
+            HashMap<Integer, Account> bankarropen = (HashMap<Integer, Account>) oIn.readObject();
+
+            return bankarropen;
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Kunde inte öppna filen 'AccountDb");
+        } catch (IOException e) {
+            System.out.println("IOException!!! " + e);
+        } catch (ClassNotFoundException e) {
+            System.out.println("ClassNotFoundException bad luck");
+        }
+
+
+        return null;
+    }
+}
