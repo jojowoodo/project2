10f4a05663dccc32ba32f9f376c7ce38fa6810ec
jojowoodo <jojowoodo@yahoo.com>
2020-03-05 10:07:04 +0200

    Fixa till att alla languages läses in i main;

--- project2/src/com/company/Main.java
+++ project2/src/com/company/Main.java
@@ -8,12 +8,26 @@ public class Main {
     }
 
 
-    public static void TestRun(){
+    public static void TestRun() {
 
         System.out.println(FileInput.readFile("English.txt"));
-        new Language(FileInput.readFile("English.txt").replaceAll("\\e",""),"English");
+        System.out.println(FileInput.readFile("Deutch.txt"));
+        System.out.println(FileInput.readFile("Eesti.txt"));
+        System.out.println(FileInput.readFile("Francais.txt"));
+        System.out.println(FileInput.readFile("Italiano.txt"));
+        System.out.println(FileInput.readFile("Norsk.txt"));
+        System.out.println(FileInput.readFile("Suomi.txt"));
+        System.out.println(FileInput.readFile("Svenska.txt"));
+        new Language(FileInput.readFile("Suomi.txt").replaceAll("\\f", ""), "Finnish");
+        new Language(FileInput.readFile("English.txt").replaceAll("\\e", ""), "English");
+        new Language(FileInput.readFile("Suomi.txt").replaceAll("\\f", ""), "Deutch");
+        new Language(FileInput.readFile("English.txt").replaceAll("\\e", ""), "Eesti");
+        new Language(FileInput.readFile("Suomi.txt").replaceAll("\\f", ""), "Francais");
+        new Language(FileInput.readFile("English.txt").replaceAll("\\e", ""), "Italiano");
+        new Language(FileInput.readFile("Suomi.txt").replaceAll("\\f", ""), "Norsk");
+        new Language(FileInput.readFile("English.txt").replaceAll("\\e", ""), "Suomi");
+        new Language(FileInput.readFile("Suomi.txt").replaceAll("\\f", ""), "Svenska");
     }
-
 
 
 }
